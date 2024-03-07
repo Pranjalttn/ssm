ccc9e1a955f5e87adbdbd7430db43e468e339917
Pranjalttn <126054657+Pranjalttn@users.noreply.github.com>
2024-03-07 12:25:56 +0530

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -13,7 +13,7 @@ regions=("us-east-1" "us-east-2" "us-west-1" "us-west-2" "eu-west-1" "eu-west-2"
 cat /dev/null > output.txt
 for region in "${regions[@]}"
 do
-    echo "Checking SSM into server in $region..." | tee -a server.txt
+    echo "Checking SSM into server in $region..." | tee -a output.txt
     export AWS_DEFAULT_REGION=$region
-    python3 script.py | tee -a server.txt
+    python3 script.py | tee -a output.txt
 done
